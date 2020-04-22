03c5878f7b679a5d0684f9dc536224f329d0f4e4
p208p2002 <p208p2002@gmail.com>
2020-04-22 14:02:07 +0000

    Deploying to gh-pages from  @ b9ebb02ca6bf7208c10e966144979baf2f37cd70 🚀

---
+++ precache-manifest.520fc91be10ff2f93617179b447c133a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd86af7083e5442923626bc3d5404aec",
+    "url": "/ptt-HatePolitics-analyze/index.html"
+  },
+  {
+    "revision": "5933345dbfb18782a89e",
+    "url": "/ptt-HatePolitics-analyze/static/css/main.e7656f20.chunk.css"
+  },
+  {
+    "revision": "7456c96be45213add598",
+    "url": "/ptt-HatePolitics-analyze/static/js/2.8d8be91b.chunk.js"
+  },
+  {
+    "revision": "0b657c19c4cfb3d6362c7b59c64c0bc2",
+    "url": "/ptt-HatePolitics-analyze/static/js/2.8d8be91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5933345dbfb18782a89e",
+    "url": "/ptt-HatePolitics-analyze/static/js/main.32d3b308.chunk.js"
+  },
+  {
+    "revision": "f9a4868042a633242ab8",
+    "url": "/ptt-HatePolitics-analyze/static/js/runtime-main.a4558a69.js"
+  },
+  {
+    "revision": "8794e009c4a723a4763722f4af5da81d",
+    "url": "/ptt-HatePolitics-analyze/static/media/pull.8794e009.png"
+  },
+  {
+    "revision": "e0b2736a9910102c80985bd1fb8664e3",
+    "url": "/ptt-HatePolitics-analyze/static/media/push.e0b2736a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ptt-HatePolitics-analyze/precache-manifest.1e87cd6c7f125041c8a04b57e498cacc.js"
+  "/ptt-HatePolitics-analyze/precache-manifest.520fc91be10ff2f93617179b447c133a.js"
 );
 
 self.addEventListener('message', (event) => {
